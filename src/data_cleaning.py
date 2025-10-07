68acce0998eb178eb1ae9168fd2d62d69d63e766
Ml-engineer877 <projectmail877@gmail.com>
2025-10-07 21:16:47 +0530

    Updated

---
+++ src/data_cleaning.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd 
+import numpy as np
+
+project_path=os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+raw_path=os.path.join(project_path,"Data","Raw","raw stroke data.csv")
+process_dir=os.path.join(project_path,"Data","Processed")
+os.makedirs(process_dir,exist_ok=True)
+process_path=os.path.join(project_path,"Stroke Processed Data.csv")
+
+df=pd.read_csv(raw_path)
+print(df.head())
+
+def missing_value(df):
+    print("Checking Missing Values:")
+    print(df.isnull().sum())
+    df['bmi']=df['bmi'].fillna(df['bmi'].median())
+    print(df.isnull().sum())
+
+def duplicate(df):
+    dup=df.duplicated().sum()
+    print("Check for duplicates:",dup)
+    if dup > 0:
+        df=df.drop_duplicates()
+        print("Duplicates Removed:",df.shape)
+
+def outliers(df):
+    num_col=df[["age","avg_glucose_level"]]
+    for col in num_col:
+        q1=df[col].quantile(0.25)
+        q3=df[col].quantile(0.75)
+        iqr=q3-q1
+        outlier=df[(df[col]<q1-1.5*iqr) | (df[col]>q3+1.5*iqr)]
+    print("Outliers",outlier)
+
+
+def handle_outlier(df):
+    num_col=df[["age","avg_glucose_level"]]
+    for col in num_col:
+        q1=df[col].quantile(0.25)
+        q3=df[col].quantile(0.75)
+        iqr=q3-q1
+        df[col]=df[col].clip(lower=q1-1.5*iqr,upper=q3+1.5*iqr)
+        outlier=df[(df[col]<q1-1.5*iqr) | (df[col]>q3+1.5*iqr)]
+    print("outliers:",outlier)
+
+def visualize_data(df):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    img_path=os.path.join(project_path,"images")
+    plt.figure(figsize=(6,4))
+    sns.boxplot(df[["age","avg_glucose_level"]])
+    plt.title("After Outliers")
+    plt.savefig(os.path.join(img_path,"Without Outliers.png"))
+    plt.show()
+
+def save(df):
+    df.to_csv(process_path,index=False)
+    print("Cleaned File Is Saved To The Path:",process_path)
+
+
+missing_value(df)
+duplicate(df)
+outliers(df)
+handle_outlier(df)
+visualize_data(df)
+save(df)
---
+++ src/data_exploration.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import pandas as pd
+
+project_path=os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+raw_path=os.path.join(project_path,"Data","Raw","raw stroke data.csv")
+images_dir="../images"
+os.makedirs(images_dir,exist_ok=True)
+
+df=pd.read_csv(raw_path)
+print(df.head())
+
+def info(df):
+    print(df.head(5))
+    print(df.info())
+    print(df.describe())
+
+def missing_value(df):
+    print("Missing Values Per Column:",df.isnull().sum())
+    print("Stroke Count")
+    print(df["stroke"].value_counts(dropna=False))
+
+def visualize_data(df):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    plt.figure(figsize=(6,4))
+    sns.countplot(data=df,x="stroke")
+    plt.title("Distribution Of Strokes")
+    plt.savefig(os.path.join(images_dir,"Dis Of Strokes.jpg"))
+    plt.show()
+
+    plt.figure(figsize=(6,4))
+    sns.boxplot(df[["age","avg_glucose_level"]])
+    plt.title("Box Plot Of Numeric Columns")
+    plt.savefig(os.path.join(images_dir,"Box Plot Of Num Col.jpg"))
+    plt.show()
+
+info(df)
+missing_value(df)
+visualize_data(df)
+    
